2eb62f5a5d2217df802310da0333549e6a6e02b0
unknown <flipe282000@gmail.com>
2022-08-12 00:14:53 -0500

    \listo para produccion

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,9 +8,11 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  
   constructor(private router:Router) { }
 
   ngOnInit(): void {
+    
   }
 
   buscarProducto(termino:string){
