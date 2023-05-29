5353683d0e440598f67907a99325e4243d35740a
felipebuitrago <49107226+felipebuitrago@users.noreply.github.com>
2023-05-29 17:46:14 -0500

    whithout hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,16 @@ import { ItemComponent } from './pages/item/item.component';
 import { SearchComponent } from "./pages/search/search.component";
 
 const app_routes : Routes = [
-    { path: 'home', component : PortafolioComponent },
+    { path: '', component : PortafolioComponent },
     { path: 'about', component : AboutComponent },
     { path: 'item/:id', component : ItemComponent },
     { path: 'search/:termino', component : SearchComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+    { path: '**', pathMatch: 'full', redirectTo: '' }
 ]
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(app_routes, {useHash : true})
+        RouterModule.forRoot(app_routes)
     ],
     exports: [
         RouterModule
